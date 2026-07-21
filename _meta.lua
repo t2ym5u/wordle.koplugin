a2896b4d5566bf4af40e916add12bc3a6757386d
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 15:49:31 +0200

    chore: bump version to 1.1.4

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name        = "wordle",
     fullname    = _("Wordle"),
     description = _("Guess the hidden word in 6 tries. Letter hints guide each attempt."),
-    version     = "1.1.3",
+    version     = "1.1.4",
 }
